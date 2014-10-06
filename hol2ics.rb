c8201c09c982e6aa1a421ab5ce91ebba6e646d0a
Erwan David <erdavid@cisco.com>
2014-10-06 16:08:11 +0200

    hol2ics fonctionne en mode simple :
    -> 1 fichier en entrée, 1 fichier en sortie avec la section du hol en préfixe du nom de l'évènement.
    
    -> Todo : gérer les cas où le fichier d'entrée n'existe pas ou où le fichier de sortie ne peut pas être écrit.

--- hol2ics.rb
+++ hol2ics.rb
@@ -14,62 +14,88 @@ PRODID="-//Hol2Ics//EN"
 
 holFile=ARGV[0]
 
+class HolEvent
+  def initialize(line)
+    comp=line.strip.split(',')
+    @date=Date.parse(comp[-1])
+    @summary=comp[0..-2].join(',')
+  end
 
-class HolFile
-  def initialize(io) 
-    
+  def toVevent(summaryPrefix)
+    summary=summaryPrefix+": "+@summary
+    lendemain=@date+1
+    evtString=sprintf("BEGIN:VEVENT\n"+
+                      "SUMMARY:%s\n"+
+                      "DTSTART;VALUE=DATE:%04d%02d%02d\n"+
+                      "DTEND;VALUE=DATE:%04d%02d%02d\n"+
+                      "END:VEVENT\n",
+                      summary,
+                      @date.year,@date.month,@date.day,
+                      lendemain.year,lendemain.month,lendemain.day)
+    return evtString    
   end
 end
 
-#################### OLD Juste là pour garder des idées #######################################
-if holFile =~ /\.hol$/ then
-  basename=holFile[0..-5]
-else
-  basename=holFile
+class HolSection
+  attr_accessor :events
+  attr_reader :name
+
+  def initialize(name)
+    @name=name
+    @events=[]
+  end
+
+  def add(event)
+    @events << event
+  end
+  
 end
 
-if File.exists?(basename) then
-  warn "#{basename} existe, merci de le supprimmer"
-  exit 2
-else
-  FileUtils.mkdir(basename)
+class HolFile
+  def initialize(io)
+    @sections=[]
+    curSec=nil
+    io.each_line{|line|
+      line.strip!
+      case line
+      when /^\[(.*)\]/
+        sectionName=$1
+        curSec = HolSection.new(sectionName)
+        @sections << curSec
+      when  /^\w*$/
+        next
+      else
+        curSec.add(HolEvent.new(line))
+      end
+    }
+  end
+
+  def to_ics
+    res=""
+    @sections.each{|section|
+      section.events.each{|event|
+        res = res + event.toVevent(section.name)
+      }
+    }
+    return res
+  end
+
 end
 
+#######
+# main Prog
 
+holFile=ARGV[0]
+icsFileName=File.basename(holFile,".hol")+".ics"
+
+holData=nil
 
 File.open(holFile){|f|
+  holData=HolFile.new(f)
+}
 
-  curdef=""
-  curname=""
-
-  FileUtils.chdir(basename)
-  f.each_line{|line|
-    line=line.strip
-    case line
-    when /^\w*$/
-      next
-    when /^\[(.*)\]/
-      newname=$1
-      if curdef != "" then
-        File.open("#{curname}.ics","w"){|out|
-          out.write(curdef)
-          out.write("END:VCALENDAR\n")
-        }
-      end
-      curname=newname
-      curdef="BEGIN:VCALENDAR\nPRODID:#{PRODID}\n"
-    else
-      comp=line.strip.split(',')
-      date=comp[-1]
-      lendemain=(Date.parse(date)+1)
-      an,mois,jour=date.split('/')
-      summary=comp[0..-2].join(',')
-      curdef=curdef+
-        "BEGIN:VEVENT\n"+
-        "SUMMARY:#{summary}\n"+
-        "DTSTART;VALUE=DATE:#{an}#{mois}#{jour}\n"+
-        "DTEND;VALUE=DATE:#{lendemain.year}#{lendemain.month}#{lendemain.day}\n"+
-        "END:VEVENT\n"
-    end
-  }
+File.open(icsFileName,"w"){|out|
+  out.printf("BEGIN:VCALENDAR\nPRODID:%s\n",PRODID)
+  out.printf("%s",holData.to_ics)
+  out.printf("END:VCALENDAR\n")
 }
